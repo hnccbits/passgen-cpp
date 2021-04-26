7a36da5968ad3f4e473e3315dc0d5457cdb06a0e
SURAJ KUMAR BARNWAL <surajbarnwal24@gmai.com>
2021-04-26 16:21:55 +0530

    Added -L option

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
-    std::cout << "Hello,World!" << std::endl;
+    // set a limit of the number of arguments to be passed: in argc
+    if(argc > 4)
+    {
+        // tell the user how to run the program
+        std::cout<<"Usage1: passgen -L length_of_the_password_you_want\n";
+        // We can add other usage messages here based on the input error committed by the user.
+        return 1;
+    }
+
+    // set the length of the character set
+    if(argv[1] == "-L")
+    {
+        int length = atoi(argv[2]);
+        // generate length using length generator.
+
+    }
+    
     return 0;
 }
