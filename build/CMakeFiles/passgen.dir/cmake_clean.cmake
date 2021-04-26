ce064bb2748c570ed216cd1040fa94d75a3b9ba3
Divyansh sahu <divyanshsahu80@gmail.com>
2021-04-26 14:43:08 +0530

    -V option added

---
+++ build/CMakeFiles/passgen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/passgen.dir/src/main.cpp.o"
+  "passgen"
+  "passgen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/passgen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+double getversion()
+{
+    double ver = 1.0;
+    return ver;
+}
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]){
-    std::cout << "Hello,World!" << std::endl;
+    for (int i = 0; i < argc; ++i)
+    {
+        if (std::string(argv[i]) == "-V")
+        {
+            std::cout << "Current version: " << getversion() << '\n';
+        }
+    }
     return 0;
 }
