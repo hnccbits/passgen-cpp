f242cd84af7c35acb649805e414cf0c2026101db
Divyansh sahu <divyanshsahu80@gmail.com>
2021-04-27 17:49:09 +0530

    Removed blank lines from the end

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "version.h"
 
 int main(int argc, char *argv[]){
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i){
         if (std::string(argv[i]) == "-V"){
             std::cout << "passgen version: " << passgen_VERSION_MAJOR << "." << passgen_VERSION_MINOR  << "."<< passgen_VERSION_REVISION << "\n";
         }
