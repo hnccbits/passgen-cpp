36732130392f3b5f80f8a265abd3dea0f072327c
SURAJ KUMAR BARNWAL <surajbarnwal24@gmai.com>
2021-04-27 17:29:28 +0530

    FIXED opening braces on new line

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 
 int main(int argc, char *argv[]){
     // set a limit of the number of arguments to be passed: in argc
-    if(argc > 4)
-    {
+    if(argc > 4){
         // tell the user how to run the program
         std::cout<<"Usage1: passgen -L length_of_the_password_you_want\n";
         // We can add other usage messages here based on the input error committed by the user.
@@ -13,8 +12,7 @@ int main(int argc, char *argv[]){
 
     // set the length of the character set to default, which is 8.
     int length = 8;
-    for(int i = 0; i<argc; i++)
-    {
+    for(int i = 0; i<argc; i++){
         if(strcmp(argv[i], "-L") == 0)
             length = atoi(argv[i+1]);
     }
