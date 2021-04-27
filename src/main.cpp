f525d160a3e813209ca6e14db19d23f8d19c375e
Divyansh sahu <divyanshsahu80@gmail.com>
2021-04-27 17:39:53 +0530

    Fixed the opening braces on new line

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,8 @@
 #include "version.h"
 
 int main(int argc, char *argv[]){
-    for (int i = 0; i < argc; ++i)
-    {
-        if (std::string(argv[i]) == "-V")
-        {
+    for (int i = 0; i < argc; ++i) {
+        if (std::string(argv[i]) == "-V"){
             std::cout << "passgen version: " << passgen_VERSION_MAJOR << "." << passgen_VERSION_MINOR  << "."<< passgen_VERSION_REVISION << "\n";
         }
     }
