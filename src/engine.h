b6439e07f849ca121318b5f313836b130a0309c7
Divas Gupta <dvsmathur7050@gmail.com>
2021-05-11 20:59:39 +0530

    fixed indentation & romove stdint.h

--- src/engine.h
+++ src/engine.h
@@ -1,20 +1,20 @@
-//Define this class here.
-#include <stdint.h>
-#include <iostream>
+// Declare this class here
+#include<iostream>
 class RandomEngine {
     private:
-    uint8_t length;
-    bool symb;
+         uint8_t length=0;
+         bool symb=false;
     public:
-    //constructors:
-    RandomEngine();
-    RandomEngine(uint8_t l);
-    RandomEngine(bool s);
-    RandomEngine(uint8_t l,bool s);
-    //member functions:
-    void setLength(uint8_t);
-    void setSymbol(bool);
-    std::string getString();
-    int getLength();
-    bool getSymbolStatus();
+    // constructors:
+         RandomEngine();
+         RandomEngine(uint8_t l);
+         RandomEngine(bool s);
+         RandomEngine(uint8_t l,bool s);
+
+    // member functions:
+         void setLength(uint8_t);
+         void setSymbol(bool);
+         std::string getString();
+         int getLength();
+         bool getSymbolStatus();
 };
