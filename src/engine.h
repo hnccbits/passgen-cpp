97785aa5d552cd360383192ae2cec8a704d18f74
Divas Gupta <dvsmathur7050@gmail.com>
2021-05-11 11:28:38 +0530

    defined RamdomEngine class

--- src/engine.h
+++ src/engine.h
@@ -1,4 +1,20 @@
-// Define this class here.
+//Define this class here.
+#include <stdint.h>
+#include <iostream>
 class RandomEngine {
-
+    private:
+    uint8_t length;
+    bool symb;
+    public:
+    //constructors:
+    RandomEngine();
+    RandomEngine(uint8_t l);
+    RandomEngine(bool s);
+    RandomEngine(uint8_t l,bool s);
+    //member functions:
+    void setLength(uint8_t);
+    void setSymbol(bool);
+    std::string getString();
+    int getLength();
+    bool getSymbolStatus();
 };
