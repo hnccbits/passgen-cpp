57646136c38cfd099af114f7085ede0acdabcce3
Divyansh Sahu <work.divyansh.sahu@gmail.com>
2021-07-22 17:41:30 +0530

    Handled error for opening /dev/urandom file

--- src/engine.cpp
+++ src/engine.cpp
@@ -23,7 +23,7 @@ RandomEngine::RandomEngine():length((uint8_t)8),symb(false),mod(symb?73:63){
     //this part is independent of user's input, hence defined in the default contructor.
     std::ifstream file("/dev/urandom", std::ios::in | std::ios::binary);
     if(!file){
-    std::cout << "Error opening file\n";
+    throw std::runtime_error("Error opening file\n");
     }
     file.read(seq, sizeof(char)*100);
     file.close();
@@ -34,7 +34,7 @@ RandomEngine::RandomEngine():length((uint8_t)8),symb(false),mod(symb?73:63){
 RandomEngine::RandomEngine(uint8_t l,bool s): length(l), symb(s),mod(symb?73:63){
     std::ifstream file("/dev/urandom", std::ios::in | std::ios::binary);
     if(!file){
-    std::cout << "Error opening file\n";
+    throw std::runtime_error("Error opening file\n");
     }
     file.read(seq, sizeof(char)*100);
     file.close();
@@ -45,7 +45,7 @@ RandomEngine::RandomEngine(uint8_t l,bool s): length(l), symb(s),mod(symb?73:63)
 RandomEngine::RandomEngine(uint8_t l):length(l),mod(symb?73:63){
     std::ifstream file("/dev/urandom", std::ios::in | std::ios::binary);
     if(!file){
-    std::cout << "Error opening file\n";
+    throw std::runtime_error("Error opening file\n");
     }
     file.read(seq, sizeof(char)*100);
     file.close();
@@ -56,7 +56,7 @@ RandomEngine::RandomEngine(uint8_t l):length(l),mod(symb?73:63){
   RandomEngine::RandomEngine(bool s): symb(s),mod(symb?73:63){
      std::ifstream file("/dev/urandom", std::ios::in | std::ios::binary);
     if(!file){
-    std::cout << "Error opening file\n";
+    throw std::runtime_error("Error opening file\n");
     }
     file.read(seq, sizeof(char)*100);
     file.close();
